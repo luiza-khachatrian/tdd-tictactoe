46aebcc00a77459cf39401fef6bf50fd717d3858
luiza-khachatrian <luizakhachatrian@gmail.com>
2026-05-28 11:00:17 +0300

    Итерация 2: на доске 9 клеток

--- test_game.py
+++ test_game.py
@@ -7,3 +7,7 @@ class TestTicTacToe(unittest.TestCase):  # класс с тестами, нас�
     def test_board_creation(self):  # тест №1: проверяем что игровое поле создаётся
         game = TicTacToe()  # создаём объект игры
         self.assertIsNotNone(game.board)  # проверяем что поле существует (не None)
+
+    def test_board_has_9_cells(self):  # тест №2: поле должно иметь 9 клеток
+        game = TicTacToe()
+        self.assertEqual(len(game.board), 9)  # проверяем что длина списка равна 9
